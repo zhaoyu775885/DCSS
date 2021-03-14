aea14b217abfabe56d4b93a3c4b767c5ddf72a0b
jeremy <zhaoyu775885@163.com>
2021-03-14 22:39:40 +0800

    ...

--- datasets/imagenet.py
+++ datasets/imagenet.py
@@ -6,6 +6,7 @@ import datasets.search_imagenet as dataset_search
 from timeit import default_timer as timer
 
 
+"""
 class ImageNet:
     def __init__(self, data_dir):
         self.dataset_fn_base = torchvision.datasets.ImageNet
@@ -39,42 +40,43 @@ class ImageNet:
 
         return torch.utils.data.DataLoader(imagenet_dataset, batch_size=batch_size, shuffle=is_train, pin_memory=True,
                                            num_workers=4*torch.cuda.device_count())
+"""
 
 
-# class ImageNet:
-#     def __init__(self, data_dir):
-#         self.dataset_fn_base = torchvision.datasets.ImageNet
-#         self.data_dir = data_dir
-#         self.n_class = 1000
-#
-#     def build_dataloader(self, batch_size, is_train=True, valid=False, search=False):
-#         normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
-#
-#         train_transform = transforms.Compose([transforms.RandomResizedCrop(224),
-#                                               transforms.RandomHorizontalFlip(),
-#                                               transforms.ToTensor(),
-#                                               normalize])
-#
-#         valid_transform = transforms.Compose([transforms.Resize(256),
-#                                               transforms.CenterCrop(224),
-#                                               transforms.ToTensor(),
-#                                               normalize])
-#
-#         train_dataset = torchvision.datasets.ImageFolder(
-#             os.path.join(self.data_dir, 'train' if is_train else 'val'),
-#             transform=train_transform if is_train else valid_transform)
-#
-#         train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
-#         train_loader = torch.utils.data.DataLoader(train_dataset,
-#                                                    batch_size=batch_size,
-#                                                    num_workers=8,
-#                                                    pin_memory=True,
-#                                                    sampler=train_sampler)
-#
-#         if torch.cuda.device_count() > 1:
-#             return train_loader
-#
-#         return train_loader
+class ImageNet:
+    def __init__(self, data_dir):
+        self.dataset_fn_base = torchvision.datasets.ImageNet
+        self.data_dir = data_dir
+        self.n_class = 1000
+
+    def build_dataloader(self, batch_size, is_train=True, valid=False, search=False):
+        normalize = transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+
+        train_transform = transforms.Compose([transforms.RandomResizedCrop(224),
+                                              transforms.RandomHorizontalFlip(),
+                                              transforms.ToTensor(),
+                                              normalize])
+
+        valid_transform = transforms.Compose([transforms.Resize(256),
+                                              transforms.CenterCrop(224),
+                                              transforms.ToTensor(),
+                                              normalize])
+
+        train_dataset = torchvision.datasets.ImageFolder(
+            os.path.join(self.data_dir, 'train' if is_train else 'val'),
+            transform=train_transform if is_train else valid_transform)
+
+        train_sampler = torch.utils.data.distributed.DistributedSampler(train_dataset)
+        train_loader = torch.utils.data.DataLoader(train_dataset,
+                                                   batch_size=batch_size,
+                                                   num_workers=8,
+                                                   pin_memory=True,
+                                                   sampler=train_sampler)
+
+        if torch.cuda.device_count() > 1:
+            return train_loader
+
+        return train_loader
 
 if __name__ == '__main__':
     torch.distributed.init_process_group(backend='nccl')
